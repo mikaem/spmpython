7f6abfcaf6aa14d3b64fde67e74fd426e50eb485
Mikael Mortensen <mikaem@math.uio.no>
2017-02-07 14:18:14 +0100

    Using toeplitz

--- program2.py
+++ program2.py
@@ -1,8 +1,8 @@
 from numpy import *
 import matplotlib.pyplot as plt
-from scipy.sparse import diags
+from scipy.linalg import toeplitz
 
-Nvec = arange(2,100,2)
+Nvec = arange(2, 100, 2)
 plt.figure()
 errors = []
 for N in Nvec:
@@ -10,10 +10,8 @@ for N in Nvec:
     x = linspace(-pi, pi, N+1)[:-1]
     u = exp(sin(x))
     uprime = cos(x)*u
-    d = 0.5/tan(arange(1, N)*h/2.)
-    ds = [(-1.)**((i-1)%2)*d[i-1]*ones(N-i) for i in range(1, N)]
-    D = diags(ds, range(1, N))
-    D = (D-D.T)
+    col = hstack((0, 0.5*(-1.)**(arange(1, N))/tan(arange(1, N)*h/2.)))
+    D = toeplitz(col, -col)
     error = linalg.norm(D.dot(u)-uprime, inf)
     errors.append(error)
 
